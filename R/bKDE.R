e77336c4526faf400982126683cc3608c2d86f44
Tyler D. Rudolph <tylerdrudolph@gmail.com>
2023-03-08 12:05:06 -0500

    update messaging and arg names

--- R/bKDE.R
+++ R/bKDE.R
@@ -4,8 +4,8 @@
 #' @param id vector defining unique sample animals from which individual GPS relocations were derived.
 #' @param wts vector defining weights to be attributed to individual relocations. If the sum of relocation weights does not equal the sample size, weights are adjusted accordingly and a warning is issued.
 #' @param ncores number of threads (processes) over which to dispatch individual kernel density estimators.
-#' @param user.grid dimensions of the matrix over which the kernel will be estimated. Mandatory.
-#' @param bw.global logical indicating whether bandwidth smoothing should be derived from all relocations (recommended) or made to vary according to individual sample (point pattern) distributions.
+#' @param userGrid dimensions of the matrix over which the kernel will be estimated. Mandatory.
+#' @param bwGlobal logical indicating whether bandwidth smoothing should be derived from all relocations (recommended) or made to vary according to individual sample (point pattern) distributions.
 #' @param verbose logical indicating whether messages should be printed
 #' @param write2file logical indicating whether results should be written to file
 #'
@@ -15,7 +15,7 @@
 #' @examples
 #'
 bKDE <- function(xy, id, wts = NULL, ncores = parallel::detectCores() - 1,
-                 user.grid = NULL, bw.global = TRUE, write2file = FALSE,
+                 userGrid = NULL, bwGlobal = TRUE, write2file = FALSE,
                  verbose = TRUE) {
 
   if(nrow(xy) != length(id)) stop("id must be of length equal to nrow(xy)")
@@ -24,7 +24,7 @@ bKDE <- function(xy, id, wts = NULL, ncores = parallel::detectCores() - 1,
   } else {
     if(length(wts) != nrow(xy) | !inherits(wts, 'numeric')) stop('wts must be a vector of numeric weights of length equal to nrow(xy)')
   }
-  if(is.null(user.grid)) stop("user.grid must be provided")
+  if(is.null(userGrid)) stop("userGrid must be provided")
 
   kernelUDs = list()
   levz <- unique(id)
@@ -47,11 +47,11 @@ bKDE <- function(xy, id, wts = NULL, ncores = parallel::detectCores() - 1,
       ## kernel density estimation using with or without spatial weights (scaled to sum to 1)
       wt <- wts[id %in% levz[m]]
       wt <- length(wt) * wt / sum(wt)
-      if(bw.global) H <- Hpi(xy) else H <- Hpi(tempxy)
+      if(bwGlobal) H <- Hpi(xy) else H <- Hpi(tempxy)
       kmat <- kde(tempxy, w=wt, H=H,
-                      xmin = c(user.grid$range.x[[1]][1], user.grid$range.x[[2]][1]),
-                      xmax = c(user.grid$range.x[[1]][2], user.grid$range.x[[2]][2]),
-                      gridsize = user.grid$grid.size[1])
+                      xmin = c(userGrid$range.x[[1]][1], userGrid$range.x[[2]][1]),
+                      xmax = c(userGrid$range.x[[1]][2], userGrid$range.x[[2]][2]),
+                      gridsize = userGrid$grid.size[1])
 
       kernelUDs = c(kernelUDs, list(list(x1 = kmat$eval.points[[1]], x2 = kmat$eval.points[[2]], fhat = kmat$estimate)))
 
@@ -60,8 +60,8 @@ bKDE <- function(xy, id, wts = NULL, ncores = parallel::detectCores() - 1,
   } else {
 
     cl <- parallelly::makeClusterPSOCK(ncores, default_packages = c('sf','dplyr','terra','ks'))
-    parallel::clusterExport(cl, varlist=c('levz','xy','id','wts','bw.global',
-                                'verbose','user.grid'), envir=environment())
+    parallel::clusterExport(cl, varlist=c('levz','xy','id','wts','bwGlobal',
+                                'verbose','userGrid'), envir=environment())
 
     kernelUDs <- parallel::parLapply(cl, 1:length(levz), function(m) {
 
@@ -70,11 +70,11 @@ bKDE <- function(xy, id, wts = NULL, ncores = parallel::detectCores() - 1,
       ## Kernel density estimation
       wt <- wts[id %in% levz[m]]
       wt <- length(wt) * wt / sum(wt)
-      if(bw.global) H <- ks::Hpi(xy) else H <- ks::Hpi(tempxy)
+      if(bwGlobal) H <- ks::Hpi(xy) else H <- ks::Hpi(tempxy)
       kmat <- ks::kde(tempxy, w=wt, H=H,
-                  xmin = c(user.grid$range.x[[1]][1], user.grid$range.x[[2]][1]),
-                  xmax = c(user.grid$range.x[[1]][2], user.grid$range.x[[2]][2]),
-                  gridsize = user.grid$grid.size[1])
+                  xmin = c(userGrid$range.x[[1]][1], userGrid$range.x[[2]][1]),
+                  xmax = c(userGrid$range.x[[1]][2], userGrid$range.x[[2]][2]),
+                  gridsize = userGrid$grid.size[1])
 
       return(list(x1=kmat$eval.points[[1]], x2=kmat$eval.points[[2]], fhat=kmat$estimate))
 
